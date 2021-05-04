5b36b0bb33379ae9fb2565a77ac4bea73621b440
llbalkall <csanalosibalazs@gmail.com>
2021-05-04 11:16:13 +0200

    Revert "Merge pull request #1 from sebinemeth/Figures"
    This reverts commit bc89b1b0fb10074aa7ec340ab00b9bbd92fb71ea, reversing
    changes made to 6fb1ef6b3414996df6edaad60f81386d6746489a.

--- figure/Peasant.py
+++ figure/Peasant.py
@@ -1,7 +1,6 @@
 from figure.Figure import Figure
 from data_classes.SimplifiedBoard import SimplifiedBoard
-from utils.balazs_utils import is_enemy_field
-from utils.balazs_utils import is_empty_field
+from enums.FieldOccupation import FieldOccupation
 from player.Player import Player
 
 from typing import List
@@ -12,18 +11,15 @@ class Peasant(Figure):
         super(Figure, self).__init__(owner)
 
     def collect_possible_steps(self, simple_board: SimplifiedBoard) -> List[(int, int)]:
-        if is_empty_field(self.x + self.owner.direction_signed_1, self.y, simple_board):
+        if simple_board[self.x + self.owner.direction_signed_1][self.y] == FieldOccupation.EMPTY:
             return [(self.x + self.owner.direction_signed_1, self.y)]
         else:
             return []
 
     def collect_possible_attacks(self, simple_board: SimplifiedBoard) -> List[(int, int)]:
         ret = []
-        candidate_x = self.x + self.owner.direction_signed_1
-        candidate_y = self.y + 1
-        if is_enemy_field(candidate_x, candidate_y, simple_board):
-            ret.append((candidate_x, candidate_y))
-        candidate_y = self.y - 1
-        if is_enemy_field(candidate_x, candidate_y, simple_board):
-            ret.append((candidate_x, candidate_y))
+        if simple_board[self.x + self.owner.direction_signed_1][self.y + 1] == FieldOccupation.ENEMY:
+            ret.append((self.x + self.owner.direction_signed_1, self.y + 1))
+        if simple_board[self.x + self.owner.direction_signed_1][self.y - 1] == FieldOccupation.ENEMY:
+            ret.append((self.x + self.owner.direction_signed_1, self.y - 1))
         return ret
