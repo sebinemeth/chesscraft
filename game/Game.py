b91e83e6d38e26a0fb984e0ec620e798b4a14cae
llbalkall <csanalosibalazs@gmail.com>
2021-05-05 20:31:05 +0200

    restore the Game and kill all the state pattern

--- game/Game.py
+++ game/Game.py
@@ -1,13 +1,8 @@
-from game.AbstractGameState import AbstractGameState
-from game.FinishingState import FinishingState
-from game.RunningState import RunningState
-from game.WaitingForPlayersState import WaitingForPlayersState
+from board.Board import Board
 
 
 class Game:
-    # TODO: states can be singletons as well?
     __instance = None
-    _state = None
 
     @staticmethod
     def get_instance():
@@ -22,16 +17,8 @@ class Game:
             raise Exception("This class is a singleton!")
         else:
             Game.__instance = self
-            self.running_state = RunningState(self)
-            self.finishing_state = FinishingState(self)
-            self.waiting_for_players_state = WaitingForPlayersState(self)
-            self.transition_to(self.waiting_for_players_state)
+            self.__board = Board()
 
-    def update(self, events, pressed_keys) -> bool:
-        return self._state.update(events, pressed_keys)
-
-    def transition_to(self, state: AbstractGameState):
-        """
-        The Game allows changing the GameState object at runtime.
-        """
-        self._state = state
+    @property
+    def board(self):
+        return self.__board
