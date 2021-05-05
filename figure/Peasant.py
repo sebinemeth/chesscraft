e3b13b93396e822052c6a9343f98863ace98cff7
llbalkall <csanalosibalazs@gmail.com>
2021-05-05 15:58:41 +0200

    No dataclass:  not error, but worse code quality

--- figure/Peasant.py
+++ figure/Peasant.py
@@ -10,13 +10,13 @@ class Peasant(Figure):
     def __init__(self, owner: Player):
         super(Figure, self).__init__(owner)
 
-    def collect_possible_steps(self, simple_board: SimplifiedBoard) -> List[(int, int)]:
+    def collect_possible_steps(self, simple_board: SimplifiedBoard): #-> List[(int, int)]:
         if simple_board[self.x + self.owner.direction_signed_1][self.y] == FieldOccupation.EMPTY:
             return [(self.x + self.owner.direction_signed_1, self.y)]
         else:
             return []
 
-    def collect_possible_attacks(self, simple_board: SimplifiedBoard) -> List[(int, int)]:
+    def collect_possible_attacks(self, simple_board: SimplifiedBoard): #-> List[(int, int)]:
         ret = []
         if simple_board[self.x + self.owner.direction_signed_1][self.y + 1] == FieldOccupation.ENEMY:
             ret.append((self.x + self.owner.direction_signed_1, self.y + 1))
