f9e6ffeeffd1ce85a9b3ccedd44e1ea4deaf0ea3
llbalkall <csanalosibalazs@gmail.com>
2021-05-05 17:14:54 +0200

    Revert "trying to merge 2"
    This reverts commit 4517d89535d26e9e167d4b231364addf262c3403.

--- figure/Peasant.py
+++ figure/Peasant.py
@@ -1,3 +1,37 @@
+# from figure.Figure import Figure
+# from data_classes.SimplifiedBoard import SimplifiedBoard
+# from utils.balazs_utils import is_enemy_field
+# from utils.balazs_utils import is_empty_field
+# from player.Player import Player
+#
+# from typing import List
+#
+#
+# class Peasant(Figure):
+#     def __init__(self, owner: Player):
+#         super(Figure, self).__init__()
+#         self.owner = owner
+#
+#     def collect_possible_steps(self, simple_board: SimplifiedBoard):  # -> List[(int, int)]:
+#         if is_empty_field(self.x + self.owner.direction_signed_1, self.y, simple_board):
+#             return [(self.x + self.owner.direction_signed_1, self.y)]
+#         else:
+#             return []
+#
+#     def collect_possible_attacks(self, simple_board: SimplifiedBoard):  # -> List[(int, int)]:
+#         ret = []
+#         candidate_x = self.x + self.owner.direction_signed_1
+#         candidate_y = self.y + 1
+#         if is_enemy_field(candidate_x, candidate_y, simple_board):
+#             ret.append((candidate_x, candidate_y))
+#         candidate_y = self.y - 1
+#         if is_enemy_field(candidate_x, candidate_y, simple_board):
+#             ret.append((candidate_x, candidate_y))
+#         return ret
+# 
+#     def get_all_data(self):
+#         return "P" + self.x + self.y;
+
 from figure.Figure import Figure
 from data_classes.SimplifiedBoard import SimplifiedBoard
 from enums.FieldOccupation import FieldOccupation
--- player/PlayerManager.py
+++ player/PlayerManager.py
@@ -1,3 +1,51 @@
+# from player.HumanPlayer import HumanPlayer
+# from enums.Direction import Direction
+#
+# class PlayerManager:
+#     __instance = None
+#
+#     @staticmethod
+#     def get_instance():
+#         """ Static access method. """
+#         if PlayerManager.__instance is None:
+#             PlayerManager()
+#         return PlayerManager.__instance
+#
+#     def __init__(self):
+#         """ Virtually private constructor. """
+#         if PlayerManager.__instance is not None:
+#             raise Exception("This class is a singleton!")
+#         else:
+#             PlayerManager.__instance = self
+#             self.__my_player = None
+#             self.__other_player = None
+#             self.__current_player = None
+#
+#     # region properties
+#     @property
+#     def my_player(self):
+#         return self.__my_player
+#
+#     @property
+#     def other_player(self):
+#         return self.__other_player
+#
+#     @property
+#     def current_player(self):
+#         return self.__current_player
+#     # endregion
+#
+#     def create_players(self, my_player_id, other_player_id, starting_player_id):
+#         """Should be called when starting game."""      # TODO not like this
+#         self.__my_player = HumanPlayer(Direction.LEFT)
+#         self.__other_player = HumanPlayer(Direction.RIGHT)
+#         self.__current_player = self.__my_player
+#         pass
+#
+#     def turn_passed(self):
+#         """Should be called after each turn."""
+#         pass
+
 from __future__ import annotations
 
 from player.Player import Player
