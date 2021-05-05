ed97ca3940a5917d567e02bdf178a9528781d75a
llbalkall <csanalosibalazs@gmail.com>
2021-05-05 17:10:54 +0200

    trying to merge

--- data_classes/FigureActOptions.py
+++ data_classes/FigureActOptions.py
@@ -7,8 +7,20 @@ from typing import List
 #    possible_steps: List[(int, int)] = []
 #    possible_attacks: List[(int, int)] = []
 
+# class FigureActOptions:
+#     def __init__(self, has_remaining_action=True, possible_steps=[], possible_attacks=[]):
+#         self.has_remaining_action = has_remaining_action
+#         self.possible_steps = possible_steps
+#         self.possible_attacks = possible_attacks
+
+
+#class FigureActOptions(dataclass):
+#    has_remaining_action: bool = True
+#    possible_steps: List[(int, int)] = []
+#    possible_attacks: List[(int, int)] = []
+
 class FigureActOptions:
-    def __init__(self, has_remaining_action=True, possible_steps=[], possible_attacks=[]):
-        self.has_remaining_action = has_remaining_action
-        self.possible_steps = possible_steps
-        self.possible_attacks = possible_attacks
+    def __init__(self, has_remaining_action, possible_steps, possible_attacks):
+        self.has_remaining_action = True
+        self.possible_steps = []
+        self.possible_attacks = []
