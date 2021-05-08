55d03f74892e1b7ac6826906cc3a0ca24ea26f13
lebma <marci.lebek@gmail.com>
2021-05-08 18:21:14 +0200

    gui update

--- Screen.py
+++ Screen.py
@@ -2,22 +2,15 @@
 from game.Game import Game
 from gui_widgets.field_widget import FieldWidget
 import pygame
+from player.PlayerManager import PlayerManager
 
 
 def gui_log(func):
     def console_log(*args, **kwargs):
-        print(f'Screen log: {func.__name__}', end='')
-        print('With parameters: ', end='')
-        for arg in args:
-            k = arg.split("=")[0]
-            v = arg.split("=")[1]
-            print(f"{k}={v} ", end='')
-        for kwarg in kwargs:
-            k = kwarg.split("=")[0]
-            v = kwarg.split("=")[1]
-            print(f"{k}={v} ", end='')
+        print(f'Screen log: {func.__name__} with parameters: ', end='')
+        print(args)
         return func(*args, **kwargs)
-    return console_log()
+    return console_log
 
 
 class Screen:
@@ -36,32 +29,37 @@ class Screen:
             raise Exception("This class is a singleton!")
         else:
             Screen.__instance = self
-            size = Game.get_instance().board.SIZE
-            self.__fields = tuple(tuple(FieldWidget(x, y) for y in range(size)) for x in range(size))
+            self.__fields = None
 
     @property
     def fields(self):
         return self.__fields
 
-    @gui_log
+    def init_fields(self):
+        size = Game.get_instance().board.SIZE
+        self.__fields = tuple(tuple(FieldWidget(x, y) for y in range(size)) for x in range(size))
+
     def update(self, screen, mouse):
         screen.fill((100, 100, 50))
 
-        board_fields = Game.get_instance().board.fields
-        for x in range(len(board_fields)):
-            for y in range(len(board_fields[0])):
-                if self.fields[x][y].is_over(mouse):
-                    self.fields[x][y].set_color([120, 250, 120])
-                else:
-                    self.fields[x][y].reset_color()
-                self.fields[x][y].draw(screen)
-                if board_fields[x][y].figure is not None:
-                    self.fields[x][y].drawFigure(screen, board_fields[x][y].figure)
+        my_player = PlayerManager.get_instance().my_player
+        other_player = PlayerManager.get_instance().other_player
+        if my_player is not None and other_player is not None:
+            board_fields = Game.get_instance().board.fields
+            for x in range(len(board_fields)):
+                for y in range(len(board_fields[0])):
+                    if self.fields[x][y].is_over(mouse):
+                        self.fields[x][y].set_color([120, 250, 120])
+                    else:
+                        self.fields[x][y].reset_color()
+                    self.fields[x][y].draw(screen)
+                    if board_fields[x][y].figure is not None:
+                        self.fields[x][y].drawFigure(screen, board_fields[x][y].figure)
 
     @gui_log
     def handle(self, events, pressed_keys):
         for ev in events:
-            if ev.type == pygame.quit():
+            if ev.type == pygame.QUIT:
                 return False
             # Handle mouse buttons
             if ev.type == pygame.MOUSEBUTTONDOWN:
@@ -69,3 +67,4 @@ class Screen:
                     for y in range(len(self.fields[0])):
                         if self.fields[x][y].is_over(events.pos):
                             Game.get_instance().board.field_clicked(x, y)
+        return True
--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import pygame as pg
 from Screen import Screen
 from game.Game import Game
 
+
 def main():
     """" The main function of the game. """
     FPS = 30
@@ -13,11 +14,13 @@ def main():
 
     screen = pg.display.set_mode(res)  # init screen
 
-    while "Alma":
+    while True:
         # update "roots" & pass all the events for handling (it should be chained further to lesser interested objects)
         # they can ask for exit by returning False
-        events, pressed_keys = pg.event.get(), pg.key.get_pressed()
-        Screen.get_instance().update(screen)
+        events = pg.event.get()
+        pressed_keys = pg.key.get_pressed()
+        Screen.get_instance().update(screen, pg.mouse.get_pos())
+        # if Screen.get_instance().handle(events, pressed_keys) returns false the game quits
         if not Screen.get_instance().handle(events, pressed_keys):
             break
         clock.tick(1000//FPS)
