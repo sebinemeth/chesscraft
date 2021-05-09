6f026ad8605595d3b476ae57a6de059166a11230
llbalkall <csanalosibalazs@gmail.com>
2021-05-09 15:48:33 +0200

    pawn to queen and patt

--- board/ChoosingDestinationState.py
+++ board/ChoosingDestinationState.py
@@ -1,6 +1,8 @@
 from board.AbstractBoardState import AbstractBoardState
 from data_classes.FigureActOptions import FigureActOptions
 from figure.King import King
+from figure.Peasant import Peasant
+from figure.Queen import Queen
 from player.PlayerManager import PlayerManager
 from enums.FieldOccupation import FieldOccupation
 from figure.Figure import Figure
@@ -25,6 +27,22 @@ class ChoosingDestinationState(AbstractBoardState):
         self._board.chosen_field = self.field_xy(messages["chosen_x"], messages["chosen_y"])
         self._board.acts = FigureActOptions(True, messages["possible_steps"], messages["possible_attacks"])
 
+    def patt(self):
+        summ = 0
+        for row in self._board.fields:
+            for f in row:
+                if f.figure is not None and f.figure.owner == PlayerManager.get_instance().other_player:
+                    acts = f.figure.chosen(self._board.create_simplified_board(PlayerManager.get_instance().other_player))
+                    summ += len(acts.possible_steps) + len(acts.possible_attacks)
+        return summ == 0
+
+    def change_to_queen(self):
+        for row in self._board.fields:
+            for f in row:
+                if f.figure is not None and isinstance(f.figure, Peasant) and (f.x == 0 or f.x == 7):
+                    f.add_figure(Queen(f.figure.owner))  # occupy new field
+                      # abandon old field
+
     def field_clicked(self, x: int, y: int):
         chosen_field = self.field_xy(self.__chosen_x, self.__chosen_y)
         clicked_field = self.field_xy(x, y)
@@ -32,13 +50,13 @@ class ChoosingDestinationState(AbstractBoardState):
         chosen_fig = chosen_field.figure
         action_successful = False
         if occupation == FieldOccupation.EMPTY and (x, y) in self.__possible_steps:  # step
-            chosen_field.figure.has_not_moved_yet = False
+            chosen_fig.has_not_moved_yet = False
             clicked_field.add_figure(chosen_fig)  # occupy new field
             chosen_field.remove_figure()  # abandon old field
             action_successful = True
 
         elif occupation == FieldOccupation.ENEMY and (x, y) in self.__possible_attacks:  # attack
-            chosen_field.figure.has_not_moved_yet = False
+            chosen_fig.has_not_moved_yet = False
             f = clicked_field.remove_figure()  # killing figure there
             if isinstance(f, King):
                 print("shit happens")
@@ -47,6 +65,9 @@ class ChoosingDestinationState(AbstractBoardState):
             action_successful = True
 
         if action_successful:
+            if self.patt():
+                print("Patt!!!!")
+            self.change_to_queen()
             self._board.transition_to(self._board.frozen_state)
             if isinstance(PlayerManager.get_instance().other_player, AIPlayer):
                 PlayerManager.get_instance().other_player.turn_started(self._board)
