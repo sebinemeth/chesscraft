3129e738046fd5ed8150eca665f6c533adab0530
Sebi Nemeth <>
2021-05-09 16:41:53 +0200

    q and k positions, colors

--- gui_widgets/field_widget.py
+++ gui_widgets/field_widget.py
@@ -1,6 +1,7 @@
+import pygame
+
 from gui_widgets.button import Button
 from utils.balazs_utils import figure_image_path
-import pygame
 
 FIELD_SIZE = 50
 SIDE_MARGIN = 160
@@ -10,7 +11,7 @@ TOP_MARGIN = 100
 class FieldWidget(Button):
 
     def __init__(self, x, y):
-        color = [20, 20, 20] if (x-y) % 2 == 0 else [200, 200, 200]
+        color = [120, 120, 130] if (x - y) % 2 == 0 else [200, 200, 200]
         super().__init__(color, x*FIELD_SIZE+SIDE_MARGIN, y*FIELD_SIZE+TOP_MARGIN, FIELD_SIZE, FIELD_SIZE)
         self.figure = None
         self.original_color = color
